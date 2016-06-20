ecd615f2ff29a42962f663b356bfa35f4bf8babb
Teddy Foley <tef77777@gmail.com>
2016-06-20 17:26:20 +0000

    Done.

--- ginny_invitation.rb
+++ ginny_invitation.rb
@@ -1,1 +1,7 @@
-ginny_invitation.rb
+name = "Ginny"
+day = "Sunday"
+date = "17th"
+year = "1998"
+
+percy_invitation = "The family of #{name} Weasley proudly invite you to their graduation commencement on #{day} the #{date} of May #{year}. Festivities will be held at The Burrow. See you then!"
+puts percy_invitation
