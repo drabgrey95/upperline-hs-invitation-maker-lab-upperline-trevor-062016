f8d8ad8e2574437b4f6bc8babdc01fd9d8ac7fc0
Teddy Foley <tef77777@gmail.com>
2016-06-20 17:22:09 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,1 +1,2 @@
-#Code Here!
+percy_invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!".gsub("Percy", "Ronald").gsub("Saturday the 22nd of May 1993", "Sunday the 18th of May, 1997")
+puts percy_invitation
